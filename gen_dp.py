d1be64db51cdb129053c632bd6099923ce74b031
Xenofon-Chiotopoulos <xenofonchiotopoulos@gmail.com>
2024-06-24 10:44:32 +0200

    adding new event model

---
+++ gen_dp.py
@@ -0,0 +1,56 @@
+import numpy as np
+import itertools
+import q_event_model as em
+import copy
+
+
+def generate_hamiltonian(event: em.event, params: dict):
+    lambda_val = params.get('lambda')
+    alpha = params.get('alpha')
+    beta = params.get('beta')
+
+    modules = copy.deepcopy(event.modules)
+    modules.sort(key=lambda a: a.z)
+
+    segments = [em.segment(from_hit, to_hit) for idx in range(len(modules) - 1) for from_hit, to_hit in itertools.product(modules[idx].hits, modules[idx + 1].hits)]
+
+    N = len(segments)
+    A = np.zeros((N, N))
+    A_ang = np.zeros((N, N))
+    A_bif = np.zeros((N, N))
+    
+    b = np.zeros(N)
+
+    s_ab = np.zeros((N, N))
+    for i, seg_i in enumerate(segments):
+        for j, seg_j in enumerate(segments):
+            s_ab[i, j] = int(seg_i.from_hit.module_id == 1 and seg_j.to_hit.module_id == 1)
+
+    A_inh = np.zeros((N, N))
+
+    for i, seg_i in enumerate(segments):
+        for j, seg_j in enumerate(segments):
+            if i != j:
+                vect_i = seg_i.to_vect()
+                vect_j = seg_j.to_vect()
+                cosine = np.dot(vect_i, vect_j) / (np.linalg.norm(vect_i) * np.linalg.norm(vect_j))
+
+                eps = 1e-9
+
+                if np.abs(cosine - 1) < eps:
+                    A_ang[i, j] = 1
+
+                if (seg_i.from_hit == seg_j.from_hit) and (seg_i.to_hit != seg_j.to_hit):
+                    A_bif[i, j] = -alpha
+
+                if (seg_i.from_hit != seg_j.from_hit) and (seg_i.to_hit == seg_j.to_hit):
+                    A_bif[i, j] = -alpha
+
+                A_inh[i, j] = s_ab[i, j] * s_ab[j, i] * beta
+
+    # Compute the final expression
+    A = -1 * (A_ang + A_bif + A_inh)
+
+    components = {'A_ang': -A_ang, 'A_bif': -A_bif, 'A_inh': -A_inh}
+
+    return A, b, components, segments
