c04ba618a0e5222642e250bb9d5658f52d820521
Xenofon-Chiotopoulos <xenofonchiotopoulos@gmail.com>
2024-06-25 11:29:14 +0200

    bash scripts to run

---
+++ MCTQS.sh
@@ -0,0 +1,3 @@
+source /project/bfys/Xenofon/miniconda3/bin/activate /project/bfys/Xenofon/miniconda3/envs/MCTQS_env
+cd /project/bfys/Xenofon/MCTQS/MCTS-TrackReconstruction
+python3 -c "from main import run_MCTQS; run_MCTQS($extra)"
