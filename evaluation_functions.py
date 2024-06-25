eee5e878151cd6c36d57d1875ee25719a301bd79
Vincenzo <90111277+VincenzoLipardi@users.noreply.github.com>
2024-06-25 11:03:36 +0200

    Update evaluation_functions.py

--- evaluation_functions.py
+++ evaluation_functions.py
@@ -1,8 +1,10 @@
 import heapq
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, Aer, transpile
 from vqls_tracking import VQLS_XENO, load_from_json
+from vqe import VQE
 
-def track_recon(quantum_circuit, ansatz='all', cost=False, gradient=False):
+
+def track_vqls(quantum_circuit, ansatz='all', cost=False, gradient=False):
     # Define the problem A = c_0 I + c_1 X_1 + c_2 X_2 + c_3 Z_3 Z_4
     problem = vqls_xeno
 
@@ -15,8 +17,21 @@ def track_recon(quantum_circuit, ansatz='all', cost=False, gradient=False):
     else:
         return problem.getReward(params=[0.1], quantum_circuit=quantum_circuit, ansatz=ansatz)
 
+
+def track_vqe(quantum_circuit, ansatz='all', cost=False, gradient=False):
+    problem = VQE(hamiltonian=, n_qubits=)
+    if cost and gradient:
+        raise ValueError('Cannot return both cost and gradient descent result')
+    if gradient:
+        return problem.gradient_descent(quantum_circuit=quantum_circuit)
+    if cost:
+        return problem.costFunc(params=[0.1], quantum_circuit=quantum_circuit, ansatz=ansatz)
+    else:
+        return problem.getReward(params=[0.1], quantum_circuit=quantum_circuit, ansatz=ansatz)
+
+
 # SYSTEMS OF LINEAR EQUATIONS
 pauli_string = load_from_json('pauli_string.json')
 c = load_from_json('coeffs_list.json')
-#print(c, pauli_string)
+# print(c, pauli_string)
 vqls_xeno = VQLS_XENO(c=c, pauli_string=pauli_string, qubits=4)
