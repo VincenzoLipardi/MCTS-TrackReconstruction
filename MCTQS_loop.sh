f5d73fdfe61185d5f50a651a6f577344a92487df
Vincenzo <90111277+VincenzoLipardi@users.noreply.github.com>
2024-09-06 14:14:17 +0200

    cleaning

--- MCTQS_loop.sh
+++
@@ -1,6 +0,0 @@
-# Define the path to your Python script
-python_script="/project/bfys/Xenofon/MCTQS/MCTS-TrackReconstruction/main.py"
-
-for i in {0..100}; do
-    qsub -q long -l walltime=48:00:00 -v extra=$i MCTQS.sh
-done
